027327e098e0e11653cebe7986d3a33307aeb2d8
iansu <iansucoding@hotmail.com>
2017-05-22 16:16:19 +0800

    timeline顯示改由撈取json資料動態產生 & 加上盟立工作經驗

---
+++ js/app_data.js
@@ -0,0 +1,159 @@
+var myAppData = {
+    timeline: [
+        {
+            startYear: 2017,
+            icon: { src: 'img/carrer.png', bgClass: 'cd-picture' },
+            title: '盟立自動化互聯網',
+            contents: [
+                {
+                    header: '工作內容',
+                    list: ['路燈系統', '京城銀行', '內部教學']
+                }, {
+                    header: '使用技能',
+                    list: [
+                        'ASP.NET MVC (C#)',
+                        'MSSQL',
+                        'JavaScript',
+                        'JQuery',
+                        'GIT',
+                    ]
+                }
+            ],
+        },{
+            startYear: 2014,
+            icon: { src: 'img/carrer.png', bgClass: 'cd-picture' },
+            title: '東森信息科技/森森百貨',
+            contents: [
+                {
+                    header: '工作內容',
+                    list: ['B2B/B2E供應商系統', '內部資訊管理系統', 'Web API', '需求訪談，設計架構']
+                }, {
+                    header: '使用技能',
+                    list: [
+                        'ASP.NET MVC/WebApi (C#)',
+                        'Entity Framework',
+                        'MSSQL',
+                        'JavaScript',
+                        'JQuery',
+                        'AngularJs 1.x',
+                        'ReactJs',
+                        'Facebook API',
+                        'GIT',
+                        'SVN'
+                    ]
+                }
+            ],
+        }, {
+            startYear: 2012,
+            icon: { src: 'img/carrer.png', bgClass: 'cd-picture' },
+            title: '多奇數位/大心數位',
+            contents: [
+                {
+                    header: '工作內容',
+                    list: ['活動企劃網站', '社群網站', 'Web API', '需求訪談，設計架構']
+                }, {
+                    header: '使用技能',
+                    list: [
+                        'ASP.NET MVC/WebApi (C#)',
+                        'Entity Framework',
+                        'MSSQL',
+                        'JavaScript',
+                        'JQuery',
+                        'AngularJs 1.x',
+                        'Facebook API',
+                        'GIT',
+                        'SVN']
+                }
+
+            ]
+        }, {
+            startYear: 2012,
+            icon: { src: 'img/carrer.png', bgClass: 'cd-picture' },
+            title: 'Other Short Programming Job',
+            contents: [
+                {
+                    list: ['瑪亞資訊', '瑋騰資訊']
+                }
+            ]
+        }, {
+            startYear: 2010,
+            icon: { src: 'img/cd-icon-location.svg', bgClass: 'cd-location' },
+            title: 'Working Holiday',
+            contents: [
+                {
+                    list: ['Backpacking alone in Australia']
+                }
+            ]
+        }, {
+            startYear: 2007,
+            icon: { src: 'img/carrer.png', bgClass: 'cd-picture' },
+            title: '啟耀光電',
+            contents: [
+                {
+                    header: '工作內容',
+                    list: ['MES(生產管理系統)程式設計', '紙本表單資訊化(OA系統)', '實驗室管理資訊系統', '需求訪談，設計架構']
+                }, {
+                    header: '使用技能',
+                    list: [
+                        'ASP (VBScript)',
+                        'ASP.NET WebForm (VB.NET)',
+                        'MSSQL',
+                        'JavaScript',
+                        'JQuery',
+                        'TFS',
+                    ]
+                }
+            ],
+        }, {
+            startYear: 2004,
+            icon: { src: 'img/carrer.png', bgClass: 'cd-picture' },
+            title: 'Part Time/Other Job',
+            contents: [
+                {
+                    list: [
+                        '由田新技 設備助理工程師',
+                        '康寧玻璃 倉管',
+                        '奇美電子 技術員',
+                        '當兵',
+                        '統一企業 工讀生指導',
+                        '高中職 社團指導老師'
+                    ]
+                }
+            ]
+        }, {
+            startYear: 2000,
+            icon: { src: 'img/diploma-2-256.png', bgClass: 'cd-movie' },
+            title: 'Education',
+            contents: [
+                {
+                    header: '二技 2002 ~ 2004',
+                    list: [
+                        '立德(康寧)大學 資管系',
+                        '專題：重建侏羅紀基因 Flash動畫製作',
+                        '幹部：康樂',
+                        '社團：熱舞社社長',
+                        '打工：高中職熱舞社指導老師']
+                }, {
+                    header: '二專 2004 ~ 2002',
+                    list: [
+                        '景文科大 電子科計算機組',
+                        '專題：微電腦控制',
+                        '幹部：文書',
+                        '社團：熱舞社教學'
+                    ]
+                }
+
+            ]
+        }, {
+            startYear: 1982,
+            icon: { src: 'img/cd-icon-location.svg', bgClass: 'cd-location' },
+            title: 'Born',
+            contents: [
+                {
+                    list: ['In Tainan, Taiwan']
+                }
+            ]
+        }
+    ]
+
+};
--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+	// add timeline
+	var $timeline = $('#cd-timeline'), contentHtml = '', contentListHtml = '';
+
+	myAppData.timeline.forEach(function (item) {
+		contentHtml = '';
+		item.contents.forEach(function (cont) {
+			contentListHtml = '';
+			if (cont.list.length > 1) { // make list
+				contentListHtml = '<ul class="list-group">' + ((cont.list.map(function (li) {
+					return '<li class="list-group-item">' + li + '</li>'
+				})).join('')) + '</ul>';
+			} else {
+				contentListHtml = '<p>' + cont.list[0] + '</p>';
+			}
+			if (cont.header) { // make panel
+				contentListHtml = ' <div class="panel panel-info">\
+				<div class="panel-heading"><h3 class="panel-title">' + cont.header + '</h3></div>\
+				<div class="panel-body">' + contentListHtml + '</div>\
+				</div>';
+			}
+			contentHtml += contentListHtml; // combine contents
+		});
+
+		$timeline.append('  <!-- cd-timeline-block -->\
+                            <div class="cd-timeline-block">\
+                                <div class="cd-timeline-img '+ item.icon.bgClass + '"><img src="' + item.icon.src + '" alt="Picture"></div>\
+                                <!-- cd-timeline-img -->\
+                                <div class="cd-timeline-content">\
+                                    <h2>'+ item.title + '</h2>' + contentHtml + '\
+                                    <span class="cd-date">'+ item.startYear + '</span>\
+                                </div>\
+                                <!-- cd-timeline-content -->\
+                            </div>');
+	});
+
 	var $timeline_block = $('.cd-timeline-block');
 
 	//hide timeline blocks which are outside the viewport
-	$timeline_block.each(function(){
-		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+	$timeline_block.each(function () {
+		if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
 			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
 		}
 	});
 
 	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		$timeline_block.each(function(){
-			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+	$(window).on('scroll', function () {
+		$timeline_block.each(function () {
+			if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
 				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 			}
 		});
